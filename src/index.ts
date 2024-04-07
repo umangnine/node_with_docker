956dc41dbf35bdd7fb44b9697fa6d43449ebfa8f
Umang Kathiyara <Umang@Praxinfo.com>
2024-04-07 23:52:41 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", async function (req, res) {
-  res.send("Greeting from 5minslearn");
+  res.send("Greeting from praxinfo");
 });
 
 app.listen(PORT, async function () {
